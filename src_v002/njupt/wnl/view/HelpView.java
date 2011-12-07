90058f6494dc78e1eacb17f63242a65537a4f12d
qutianshan@gmail.com <qutianshan@gmail.com@7e3255fc-059d-36e0-4150-516429b89313>
2011-12-07 16:10:32 +0000

    增加surfaceview场景切换，增加的屏幕按钮响应 问题：切换过程有点慢，较明显的刷屏

---
+++ src_v002/njupt/wnl/view/HelpView.java
@@ -0,0 +1,74 @@
+package njupt.wnl.view;
+
+import njupt.wnl.WorkNoLateActivity;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Color;
+import android.content.Context;
+
+/**
+ * 游戏中的帮助界面
+ * 
+ * @author qu
+ *
+ */
+public class HelpView extends SurfaceView  implements SurfaceHolder.Callback{
+	
+	WorkNoLateActivity father;		//Activity的引用
+	private SurfaceHolder holder;
+	private MyThread thread;		//画图线程
+	
+	public HelpView(WorkNoLateActivity father){
+		super(father);
+		this.father = father;
+		holder = this.getHolder();
+		holder.addCallback(this);	//添加Callback接口
+		
+		thread = new MyThread(holder);
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	}
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		thread.isRun = true;
+		thread.start();
+	}
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		thread.isRun = false;
+	}
+}
+
+class MyThread extends Thread{	//画图线程
+	
+	boolean isRun;
+	private SurfaceHolder holder;
+	
+	public MyThread(SurfaceHolder holder){
+		this.holder = holder;
+	}
+	@Override
+	public void run(){
+		while (isRun) {
+			Paint paint = new Paint();
+			paint.setColor(Color.BLUE);
+			paint.setTextSize(20f);
+			
+			Canvas canvas = null;
+			try{
+				canvas = holder.lockCanvas();
+				canvas.drawColor(Color.WHITE);	//清屏幕
+				canvas.drawRect(100,100,180,140,paint);
+				canvas.drawText("helpview", 100, 100, paint);
+			}catch(Exception ex){
+			}finally{
+				if(canvas != null)
+					holder.unlockCanvasAndPost(canvas);  //将画好的画布提交
+			}
+        }
+	}
+}
